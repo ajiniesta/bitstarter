8373092b9319a9a6e5ce03547ad6871969b3e90f
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2013-08-05 23:05:44 +0000

    Reading the / request from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var content = fs.readFileSync('index.html');
+//console.log("File..." + content);
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    
+    response.send(content);
 });
 
 var port = process.env.PORT || 5000;
