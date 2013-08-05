b9dc98a33487c8cc1fe27edb8142d9e02560844a
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2013-08-05 23:08:43 +0000

    converting the buffer utf8

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
     
-    response.send(content);
+    response.send(content.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
